ffcb2649022888c9507ad48cd745056c42253c2c
wannabeOG <saihimallu1010@gmail.com>
2023-03-03 22:54:52 -0500

    Preliminary test cases and CREATE function

--- service/routes.py
+++ service/routes.py
@@ -29,3 +29,64 @@ def index():
 ######################################################################
 
 # Place your REST API code here ...
+
+######################################################################
+# C R E A T E    A    N E W    C U S T O M E R
+######################################################################
+@app.route("/customers", methods=["POST"])
+def create_customers():
+    """ Creates a customer instance in the database. The info is parsed
+        from the request and the fields are populated using the appropriate
+        methods in Customer class.
+    """
+    app.logger.info("Request to create a Customer")
+    
+    # Create the customer
+    customer = Customer()
+    customer.deserialize(request.get_json())
+    customer.create()
+
+    # Create a message to return
+    message = customer.serialize()
+    #location_url = url_for("get_customers", customer_id=customer.id)
+
+    # return make_response(
+    #     jsonify(message), status.HTTP_201_CREATED, {"Location": location_url} 
+    # )
+
+    return make_response(
+        jsonify(message), status.HTTP_201_CREATED
+    )
+
+######################################################################
+# C R E A T E 
+######################################################################
+@app.route("/customers/<int:customer_id>/addresses", methods=["POST"])
+def create_addresses(customer_id):
+    """Creates an address linked to a specific customer"""
+
+    app.logger.info(f"Request to create an address for a customer with {customer_id}")
+    
+    #get the customer information
+    customer = Customer.find(customer_id)
+
+    if not customer:
+        abort(status.HTTP_404_NOT_FOUND, f"{customer_id} does not exist")
+
+    #Create an address instance for the customer = customer_id
+    address = Address()
+    address.deserialize(request.get_json())
+
+    customer.addresses.append(address)
+    customer.update()
+
+    #Create the final message that is to be sent
+    to_send = address.serialize()
+
+    return make_response(jsonify(to_send), status.HTTP_201_CREATED)
+
+
+
+
+
+
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,39 +7,188 @@ Test cases can be run with the following:
 """
 import os
 import logging
+import random
+
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 from service import app
-from service.models import db
+from service.models import db, Customer, Address, init_db
 from service.common import status  # HTTP Status Codes
 
+
+from tests.customer_factory import CustomerFactory, AddressFactory
+
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
+)
+
+BASE_URL = "/customers"
+LOWER_LIMIT = 1
+UPPER_LIMIT = 10
 
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
-class TestYourResourceServer(TestCase):
+class TestCustomerService(TestCase):
     """ REST API Server Tests """
 
     @classmethod
     def setUpClass(cls):
         """ This runs once before the entire test suite """
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        init_db(app)
 
     @classmethod
     def tearDownClass(cls):
         """ This runs once after the entire test suite """
+        pass
 
     def setUp(self):
         """ This runs before each test """
+        db.session.query(Customer).delete()
+        db.session.commit()
         self.app = app.test_client()
 
     def tearDown(self):
         """ This runs after each test """
+        db.session.remove()
 
     ######################################################################
     #  P L A C E   T E S T   C A S E S   H E R E
     ######################################################################
 
     def test_index(self):
-        """ It should call the home page """
+        """ Test case that checks if the home page is getting called"""
         resp = self.app.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+    def test_create_customer_valid_id(self):
+        """Test case to check if the Customer ID is populated appropriately; happy paths"""
+
+        customers = CustomerFactory.create_batch(random.randint(LOWER_LIMIT, UPPER_LIMIT))
+
+        for cust in customers:
+            logging.debug(cust)
+            cust_post_req = self.app.post(BASE_URL, json=cust.serialize())
+
+            #Assert that the customer has been created successfully
+            self.assertEqual(cust_post_req.status_code, status.HTTP_201_CREATED, "Customer did not get created")
+            
+            created_customer = cust_post_req.get_json()
+            self.assertIsNotNone(created_customer["id"], "IDs haven't been created")
+
+    def test_create_customer_valid_first_name(self):
+        """Test case to check if the Customer first name is populated appropriately; happy paths """
+
+        customers = CustomerFactory.create_batch(random.randint(LOWER_LIMIT, UPPER_LIMIT))
+
+        for cust in customers:
+            logging.debug(cust)
+            cust_post_req = self.app.post(BASE_URL, json=cust.serialize())
+
+            #Assert that the customer has been created successfully
+            self.assertEqual(cust_post_req.status_code, status.HTTP_201_CREATED, "Customer did not get created")
+            
+            created_customer = cust_post_req.get_json()
+            self.assertEqual(created_customer["first_name"], cust.first_name, "First names are not matching")
+
+    def test_create_customer_valid_last_name(self):
+        """Test case to check if the Customer last name is populated appropriately; happy paths"""
+
+        customers = CustomerFactory.create_batch(random.randint(LOWER_LIMIT, UPPER_LIMIT))
+
+        for cust in customers:
+            logging.debug(cust)
+            cust_post_req = self.app.post(BASE_URL, json=cust.serialize())
+
+            #Assert that the customer has been created successfully
+            self.assertEqual(cust_post_req.status_code, status.HTTP_201_CREATED, "Customer did not get created")
+            
+            created_customer = cust_post_req.get_json()
+            self.assertEqual(created_customer["last_name"], cust.last_name, "Last names are not matching")
+    
+    def test_create_customer_valid_email(self):
+        """Test case to check if the Customer email is populated appropriately; happy paths"""
+
+        customers = CustomerFactory.create_batch(random.randint(LOWER_LIMIT, UPPER_LIMIT))
+
+        for cust in customers:
+            logging.debug(cust)
+            cust_post_req = self.app.post(BASE_URL, json=cust.serialize())
+
+            #Assert that the customer has been created successfully
+            self.assertEqual(cust_post_req.status_code, status.HTTP_201_CREATED, "Customer did not get created")
+            
+            created_customer = cust_post_req.get_json()
+            self.assertEqual(created_customer["email"], cust.email, "Emails are not matching")
+
+    def test_create_customer_valid_password(self):
+        """Test case to check if the Customer password is populated appropriately; happy paths"""
+
+        customers = CustomerFactory.create_batch(random.randint(LOWER_LIMIT, UPPER_LIMIT))
+
+        for cust in customers:
+            logging.debug(cust)
+            cust_post_req = self.app.post(BASE_URL, json=cust.serialize())
+
+            #Assert that the customer has been created successfully
+            self.assertEqual(cust_post_req.status_code, status.HTTP_201_CREATED, "Customer did not get created")
+            
+            created_customer = cust_post_req.get_json()
+            self.assertEqual(created_customer["password"], cust.password, "Passwords are not matching")
+
+    def test_create_invalid_customer_valid_address_id(self):
+        """Test case to check if the address fields are not populated for a random ID"""
+
+        address = AddressFactory()
+        cust_id = -100000
+
+        logging.debug(address)
+
+        #make a post request to the address database
+        addr_post_req = self.app.post(f"{BASE_URL}/{cust_id}/addresses", json=address.serialize())
+
+        print("Address post request", addr_post_req.status_code)
+
+        #assert that the address request had been created
+        self.assertEqual(addr_post_req.status_code, status.HTTP_404_NOT_FOUND, "Address got created, should not have happened")
+
+
+    def test_create_valid_customer_valid_address_id(self):
+        """Test case to check if the address fields are populated appropriately; happy paths"""
+
+        customer = CustomerFactory()
+        logging.debug(customer)
+
+        #create the customer
+        cust_post_req = self.app.post(BASE_URL, json=customer.serialize())
+
+        #verify that the customer has been created
+        self.assertEqual(cust_post_req.status_code, status.HTTP_201_CREATED, "Customer did not get created")
+
+        created_customer = cust_post_req.get_json()
+
+        #verify that the id has been created successfully
+        self.assertIsNotNone(created_customer["id"], "IDs haven't been created")
+
+        #retrieve the ID for the customer
+        cust_id = created_customer["id"]
+
+        #create the addresses now
+
+        addresses = AddressFactory.create_batch(random.randint(LOWER_LIMIT, UPPER_LIMIT))
+
+        for addr in addresses:
+            logging.debug(addr)
+
+            #make a post request to the address database
+            addr_post_req = self.app.post(f"{BASE_URL}/{cust_id}/addresses", json=addr.serialize())
+
+            #assert that the address request had been created
+            self.assertEqual(addr_post_req.status_code, status.HTTP_201_CREATED, "Address did not get created")
+
+            created_addr = addr_post_req.get_json()
+            self.assertIsNotNone(created_customer["id"], "Addresss ID has not been populated correctly")
