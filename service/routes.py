20017c622bc0bd5b728771c8c0e1058bdf8f5936
Akshama <akshama96@gmail.com>
2023-03-24 16:39:49 -0400

    Added query ability in list_customers according to #59

--- service/routes.py
+++ service/routes.py
@@ -40,24 +40,17 @@ def list_customers():
     """Returns all of the Customers"""
     app.logger.info("Request for Customer list")
     customers = []
-
     # Process the query string if first name matches
     first_name = request.args.get("first_name")
-    if first_name:
-        customers = Customer.find_by_first_name(first_name)
-    else:
-        customers = Customer.all()
-
     # Process the query string if last name matches
     last_name = request.args.get("last_name")
-    if last_name:
-        customers = Customer.find_by_last_name(last_name)
-    else:
-        customers = Customer.all()
-
     # Process the query string if email matches
     email = request.args.get("email")
-    if email:
+    if first_name:
+        customers = Customer.find_by_first_name(first_name)
+    elif last_name:
+        customers = Customer.find_by_last_name(last_name)
+    elif email:
         customers = Customer.find_by_email(email)
     else:
         customers = Customer.all()
